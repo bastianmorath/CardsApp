32972866dad874b15e05a1a8691b96b6d6f6cc4b
Lukas Reichart <reichart@antum.ch>
2015-11-19 19:01:41 +0100

    Mock API
    Implemented Mock API for flashcards.

---
+++ app/api/flashCardLibrary.js
@@ -0,0 +1,31 @@
+/**
+ * @author    ::  Bastian Morath and Lukas Reichart
+ * @copyright ::  Bastian Morath and Lukas Reichart
+ *
+ * @flow
+ */
+
+const TIMEOUT = 100;
+import _flashcards from '../../test/mock/flashcard';
+
+/**
+ * FlashCardLibrary is the API for all data operations regarding the flashcard data
+ * objects.
+ * @type {Object}
+ */
+const FlashCardLibrary = {
+  /**
+   * Fetches all flashcards of a user from the database.
+   * At the moment this only passes our mock data.
+   * @return {Promise} [description]
+   */
+  getFlashcards() {
+    return new Promise( (resolve) => {
+      setTimeout( () => {
+        resolve( _flashcards );
+      }, TIMEOUT);
+    });
+  },
+};
+
+export default FlashCardLibrary;
