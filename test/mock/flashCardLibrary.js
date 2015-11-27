e4346b97aac5819ad85a6f7c9988359c643c1ec5
Lukas Reichart <reichart@antum.ch>
2015-11-27 13:46:31 +0100

    Implemented tests
    - implemented tests for deleting a flashcard
    - added a sepaerate file for the mock flashCardLibrary
    - stub all API function with the mock flashCardLibrary.

---
+++ test/mock/flashCardLibrary.js
@@ -0,0 +1,87 @@
+/**
+ * @author    ::  Bastian Morath and Lukas Reichart
+ * @copyright ::  Bastian Morath and Lukas Reichart
+ *
+ * @flow
+ */
+
+const TIMEOUT = 50;
+import _ from 'lodash';
+import _flashcards from './flashcard';
+let idCounter = 0;
+_.each( _flashcards, (flashcard) => {
+  flashcard.id = idCounter.toString();
+  idCounter++;
+});
+
+/**
+ * Mock FlashCardLibrary, that should define the same function as the real one.
+ * Used in the unit tests for nuclear-js.
+ * @type {Object}
+ */
+const FlashCardLibrary = {
+  /**
+   * Fetches all flashcards of a user from the database.
+   * At the moment this only passes our mock data.
+   * @return {Promise}
+   */
+  fetchFlashcards(): Promise {
+    return new Promise( (resolve) => {
+      setTimeout( () => {
+        resolve( _flashcards );
+      }, TIMEOUT);
+    });
+  },
+
+  /**
+   * Takes a single data object or array of objects and create new flashcards entry
+   * in the database.
+   * @param  {array|object} flashcards to create.
+   * @return {Promise}
+   */
+  createFlashcards( flashcards:Array<Object> ): Promise {
+    if (!flashcards) {
+      // TODO: handle the error here.
+      return Promise.reject();
+    }
+
+    let flashcardsToCreate = flashcards;
+    if (!_.isArray(flashcards)) {
+      flashcardsToCreate = [flashcards]; // eslint-disable-line no-param-reassign
+    }
+
+    return new Promise( (resolve) => {
+      setTimeout( () => {
+        _.each( flashcardsToCreate, (flashcard) => {
+          flashcard.id = idCounter.toString();
+          _flashcards.push( flashcard );
+          idCounter++;
+        });
+        resolve( flashcardsToCreate );
+      }, TIMEOUT );
+    });
+  },
+
+  /**
+   * API method: batch deletes flashcards from the database identified by the passed
+   * array of ids.
+   */
+  deleteFlashcards( flashcardIdsToDelete: Array<string> ): Promise {
+    if (!flashcardIdsToDelete) {
+      // TODO: hanlde the error here.
+      return Promise.reject();
+    }
+
+    return new Promise( (resolve) => {
+      setTimeout( () => {
+        // delete all the flashcards from the in memory map.
+        _.remove( _flashcards, (flashcard) => {
+          return _.find(flashcardIdsToDelete, flashcard.id );
+        });
+        resolve(flashcardIdsToDelete);
+      });
+    });
+  },
+};
+
+export default FlashCardLibrary;
