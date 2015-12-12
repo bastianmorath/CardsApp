cd93f6a6d59a14ba8ded26f02f15e48acac1bf77
Lukas Reichart <reichart@antum.ch>
2015-12-12 23:01:48 +0100

    Renamed folder containing nuclear tests.

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,7 +13,7 @@ module.exports = function GruntInit(grunt) {
           clearRequireCache: true,
           require: 'babel/register',
         },
-        src: ['test/data/*.js'],
+        src: ['test/nuclear/*.js'],
       },
     },
 
